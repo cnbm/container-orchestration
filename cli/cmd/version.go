4efb08a450b9abda1505a260d175e81962430e07
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-08-24 15:25:04 +0100

    adds version command to CLI

---
+++ cli/cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Displays the CNBM Container Orchestration CLI version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("%s\n", releaseVersion)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
