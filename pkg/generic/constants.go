f1855489ddc2529ac1cc4a558812f9d720e4002f
Joerg Schad <joerg@mesosphere.io>
2017-09-28 21:02:23 +0100

    Updated RunType specifications.

--- pkg/generic/constants.go
+++ pkg/generic/constants.go
@@ -27,11 +27,52 @@ const (
 	RunScaling BenchmarkRunType = "scaling"
 	
 	// RunDistribution represents the distribution benchmark run type
+	// Precondition: 
+	//	* empty cluster
+	//	* busybox:1 docker image prefetched on all nodes
+	// Benchmark: 
+	//	* scale-up N container on empty cluster
+	// Container Specification
+	//	* docker image: busybox:1 (prefetched), 
+	//      * container runtime: free to choose, potential parameter 
+	//      * cpu: 0.5
+	//      * memory: 300MB
+	// Postcondition: all container running (i.e., not necessarily ready/passing health check)
+	// Result:  map: nodeid -> set of containers  
 	RunDistribution BenchmarkRunType = "distribution"
+	
 	// RunAPICalls represents the apicalls benchmark run type
+	// Precondition: 
+	//	* 5 container running
+	//  		* Container Specification
+	//			* docker image: busybox:1 (prefetched), 
+	//      		* container runtime: free to choose, potential parameter 
+	//      		* cpu: 0.5
+	//      		* memory: 300MB
+	//			* busybox:1 docker image prefetched on all nodes
+	// Benchmark: 
+	// 	* API call listing all containers
+	// Postcondition: all container still running
+	// Result: runtime in seconds
 	RunAPICalls BenchmarkRunType = "apicalls"
+	
 	// RunSD represents the servicediscovery benchmark run type
 	RunSD BenchmarkRunType = "servicediscovery"
+	
 	// RunRecovery represents the recovery benchmark run type
+	// Precondition: 
+	//	* 1 container running
+	//  		* Container Specification
+	//			* docker image: busybox:1 (prefetched), 
+	//      		* container runtime: free to choose, potential parameter 
+	//      		* cpu: 0.5
+	//      		* memory: 300MB
+	//			* busybox:1 docker image prefetched on all nodes
+	//			* sleep 10000
+	// Benchmark: 
+	// 	* Kill the task inside the container
+	//	* Wait for deployment 
+	// Postcondition:  container running again
+	// Result: recovery time in seconds
 	RunRecovery BenchmarkRunType = "recovery"
 )
