1f91d77a430739795ecc8c5881c20606eeb33c05
Joerg Schad <joerg@mesosphere.io>
2017-09-28 19:02:19 +0100

    Added specification for scaling RunType.

--- pkg/generic/constants.go
+++ pkg/generic/constants.go
@@ -3,12 +3,29 @@ package generic
 const (
 	// DefaultOutputDir is the default output directory for results
 	DefaultOutputDir = "/tmp/cnbm/results"
+	
+	
 	// TargetDCOS represents DC/OS as the target container orchestration system
 	TargetDCOS BenchmarkTarget = "dcos"
 	// TargetK8S represents Kubernetes as the target container orchestration system
 	TargetK8S BenchmarkTarget = "kubernetes"
+	
+	
 	// RunScaling represents the scaling benchmark run type
+	// Precondition: 
+	//	* empty cluster
+	//	* busybox:1 docker image prefetched on all nodes
+	// Benchmark: 
+	//	* scale-up 3 container on empty cluster
+	// Container Specification
+	//	* docker image: busybox:1 (prefetched), 
+	//      * container runtime: free to choose, potential parameter 
+	//      * cpu: 0.5
+	//      * memory: 300MB
+	// Postcondition: all container running (i.e., not necessarily ready/passing health check)
+	// Result: start up time in seconds
 	RunScaling BenchmarkRunType = "scaling"
+	
 	// RunDistribution represents the distribution benchmark run type
 	RunDistribution BenchmarkRunType = "distribution"
 	// RunAPICalls represents the apicalls benchmark run type
